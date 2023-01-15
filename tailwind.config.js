7f4991474fd28923ac6a6cad1d22285345df8171
Takaitech <takai.tech2144@gmail.com>
2023-01-14 22:56:05 -0800

    init

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "darkGrey": "#2F2F2F"
+      }
+    },
+  },
+  plugins: [],
+}
