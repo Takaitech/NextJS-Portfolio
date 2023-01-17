6c0b0ad330329f49b449843638f427720b1fc68f
Takaitech <takai.tech2144@gmail.com>
2023-01-16 22:10:39 -0800

    translated gradient

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,32 @@
+"use client";
+
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from './page.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import { useEffect } from 'react'
 
 
 export default function Home() {
+
+  useEffect(() => {
+    let bg = document.getElementById("mask")
+    const windowWidth = window.innerWidth / 5;
+    const windowHeight = window.innerHeight / 5 ;
+    
+    document.body.addEventListener('mousemove', (e) => {
+      const mouseX = e.clientX / windowWidth;
+      const mouseY = e.clientY / windowHeight;
+      
+      bg.style.transform = `translate3d(0, -${(mouseY * mouseX) * 3}%, 0)`;
+    });
+
+  }, []);
+
+  
   return (
-    <main className='bg-darkGrey'>
+    <main  className='bg-darkGrey'>
       <div className="flex h-screen w-screen">
-        <div className='flex-1'>
+        <div className='w-6/12'>
           <h1>Taylor Ward</h1>
           <nav>
             <li>
@@ -23,9 +40,9 @@ export default function Home() {
             </li>
           </nav>
         </div>
-        <div className=" mask flex-1 w-full h-full bg-[url('/background-gradient.jpg')] bg-cover ">
-       
+        <div id="mask" className="w-[50vw] h-[125vh] absolute left-2/4 top-0  2-2vw mask grow-1 bg-[url('/background-gradient.png')] bg-cover ">
         </div>
+        <img  src="/topograph-pattern-inverse.png" className='w-6/12 h-full absolute top-0 bottom-0 right-0 '></img>
       </div>
     </main>
   )
